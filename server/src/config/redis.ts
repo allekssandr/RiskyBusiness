3f051be943bce335dae7b9470c39483df7acffc4
Aleksandr Maiorov <m.allekssandr@gmail.com>
2025-09-14 00:39:45 +0500

    Добавление типизации для клиента Redis: уточнен тип redisClient в файле redis.ts

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,6 +1,6 @@
-import { createClient } from 'redis';
+import { createClient, type RedisClientType } from 'redis';
 
-const redisClient = createClient({
+const redisClient: RedisClientType = createClient({
   url: process.env.REDIS_URL || 'redis://localhost:6379',
 });
 
